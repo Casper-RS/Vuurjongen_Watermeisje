18863bae0dd822a6c230792c8a86c776e581b5b7
PinballPRO07 <119053887+PinballPRO07@users.noreply.github.com>
2025-03-17 12:00:10 +0100

    test

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,4 +2,11 @@ module Vuurjongen.Watermeisje {
     requires hanyaeger;
     exports dev.eindopdracht;
 
+    opens audio;
+    opens backgrounds;
+    opens sprites;
+
+    exports dev.eindopdracht.scenes;
+    exports dev.eindopdracht.entities.buttons;
+    exports dev.eindopdracht.entities;
 }
