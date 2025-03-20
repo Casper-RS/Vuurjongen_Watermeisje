6ba8e36781e92497ccd71ab7e5032d05d2adfbab
PinballPRO07 <119053887+PinballPRO07@users.noreply.github.com>
2025-03-20 11:45:08 +0100

    Commited the player classes

---
+++ src/main/java/dev/eindopdracht/entities/sprite/dynamic/Fireboy.java
@@ -0,0 +1,13 @@
+package dev.eindopdracht.entities.sprite.dynamic;
+
+public class Fireboy extends Player{
+    public Fireboy(int x, int y, int speed, int jumpstrenght) {
+        super(x, y, speed, jumpstrenght);
+    }
+
+    @Override
+    public void onCollision() {
+
+    }
+}
+
