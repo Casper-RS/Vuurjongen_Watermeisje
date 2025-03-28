1f577ab1ced08af5a9822851ae24d38e8a5aa168
PinballPRO07 <119053887+PinballPRO07@users.noreply.github.com>
2025-03-28 14:56:18 +0100

    Created an levelCreator to make levels faster with less code.

---
+++ src/main/java/dev/eindopdracht/scenes/LevelCreator.java
@@ -0,0 +1,36 @@
+package dev.eindopdracht.scenes;
+
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.scenes.DynamicScene;
+import com.github.hanyaeger.api.scenes.TileMapContainer;
+import dev.eindopdracht.VuurjongenWatermeisje;
+import dev.eindopdracht.entities.sprite.dynamic.Fireboy;
+import dev.eindopdracht.entities.sprite.dynamic.Watergirl;
+
+public abstract class LevelCreator extends DynamicScene implements TileMapContainer {
+    protected VuurjongenWatermeisje vuurjongenWatermeisje;
+
+    public LevelCreator(VuurjongenWatermeisje v) {
+        this.vuurjongenWatermeisje = v;
+    }
+    @Override
+    public abstract void setupTileMaps();
+
+    @Override
+    public abstract void setupScene();
+
+    @Override
+    public abstract void setupEntities();
+
+    public void createUI() {
+
+    }
+
+    public void createCharacters() {
+        var fireboy = new Fireboy(new Coordinate2D(getWidth() / 2, getHeight() / 2), vuurjongenWatermeisje);
+        addEntity(fireboy);
+
+        var watergirl = new Watergirl(new Coordinate2D(getWidth() / 2, getHeight() / 2), vuurjongenWatermeisje);
+        addEntity(watergirl);
+    }
+}
